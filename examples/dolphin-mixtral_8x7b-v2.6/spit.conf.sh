ad0824e25cb42522bcd42ad4e1569301cb5d6bb8
Kurt Manucredo <fuzzybritches0@gmail.com>
2024-01-21 00:37:35 +0000

    add dolphin-mixtral_8x7b-v2.6 example

---
+++ examples/dolphin-mixtral_8x7b-v2.6/spit.conf.sh
@@ -0,0 +1,72 @@
+#PROMPT_TEMPLATE
+CTX_SIZE=16384
+USER_NAME="${USER^}"
+AI_NAME="Sam"
+BOS="<|im_start|>"
+EOS="<|im_end|>"
+SYS_START="${BOS} system "
+SYS_END="${EOS}\n"
+INST_START="${BOS} user "
+INST_START_NEXT=
+INST_END="${EOS}\n"
+REPL_START="${BOS} assistant"
+REPL_END="${EOS}\n"
+LLAMACPP_FIX=
+OFFSET_FIX_TOKENS=("${EOS}")
+STOP_SEQUENCES=("EXECUTE")
+
+EXECUTE() {
+	[ ! -d "./${ID}/env" ] && mkdir ./${ID}/env
+	echo -ne "\n[OUTPUT]"
+	echo "${1}" > ./${ID}/EXECUTE
+	chmod u+x ./${ID}/EXECUTE
+	cd ./${ID}/env
+	../EXECUTE 2>&1
+	EXIT_CODE="${?}"
+	rm ../EXECUTE
+	echo -ne "[/OUTPUT EXIT_CODE:${EXIT_CODE}]\n"
+}
+
+DEBUG=
+INTERACTIVE=1
+
+PROG=("./main"
+--ctx_size ${CTX_SIZE}
+--keep -1
+--repeat_last_n 512
+--repeat_penalty 1.17647
+--no-penalize-nl
+--temp 0
+--top_k 40
+--top_p 0.5
+--threads 12
+--verbose-prompt
+--model "./dolphin-2.6-mixtral-8x7b.Q8_0.gguf")
+
+SYSTEM="You are ${AI_NAME} from AI Luna Labs. You are a seasoned and professional computer software and computer system engineer. You perform tasks for ${USER_NAME} and assist them with their engineering and development efforts. You are friendly, honest, helpful, and conscientious. You have access to a Linux terminal by running scripts using the following format:
+[EXECUTE]
+{script}
+[/EXECUTE]
+
+Examples:
+
+[EXECUTE]
+#!/bin/bash
+
+echo \"Hello World!\"
+[/EXECUTE]
+[OUTPUT]Hello World!
+[/OUTPUT EXIT_CODE:0]
+
+
+[EXECUTE]
+#!/bin/python
+
+print(\"Hello World!\")
+[/EXECUTE]
+[OUTPUT]Hello World!
+[/OUTPUT EXIT_CODE:0]
+
+Use EXECUTE responsibly. When you want to EXECUTE scripts, first write the script without the EXECUTE directive and ask ${USER_NAME} for permission to EXECUTE it.
+
+Always inspect the OUTPUT carefully. If you encounter error messages or any other unexpected OUTPUT, make sure there isn't a mishap. If you discover a mishap, or ${USER_NAME} points one out to you, do not fix it right away. Instead, repeat the script exactly as it was, line by line, until you are above the line you want to fix. Now, insert a new line containing helpful information to fix the line. Do not fix the line yet, but continue repeating the original script in full. Finally repeat the script once again, this time first remove the line with the helpful information, and then fix the line that's broken. If necessary repeat this process until the script works as expected."
