639392a0b0ffc499a8f864db6019bbd2beac476c
Kurt Manucredo <fuzzybritches0@gmail.com>
2024-01-21 00:38:35 +0000

    add Mixtral-8x7B-Instruct-v0.1 example

---
+++ examples/Mixtral-8x7B-Instruct-v0.1/spit.conf.sh
@@ -0,0 +1,32 @@
+#PROMPT_TEMPLATE
+CTX_SIZE=32768
+USER_NAME="User"
+AI_NAME="Assistant"
+BOS="<s>"
+EOS="</s>"
+SYS_START=
+SYS_END=
+INST_START="${BOS} [INST] "
+INST_START_NEXT=
+INST_END=" [/INST]"
+REPL_START=
+REPL_END="${EOS}"
+LLAMACPP_FIX=
+OFFSET_FIX_TOKENS=("${BOS}" "${EOS}")
+STOP_SEQUENCES=()
+
+DEBUG=
+INTERACTIVE=1
+
+PROG=("./main"
+--ctx_size ${CTX_SIZE}
+--keep -1
+--repeat_last_n 512
+--repeat_penalty 1.17647
+--no-penalize-nl
+--temp 0.5
+--top_k 40
+--top_p 0.5
+--threads 12
+--verbose-prompt
+--model "./mixtral-8x7b-instruct-v0.1.Q8_0.gguf")
